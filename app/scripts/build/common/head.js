125df8938c5b8dc8b3cae4b065a35424537dd978
Vitaliy Tarash @ Pulse <vitaliy.tarash@gmail.com>
2015-11-04 15:36:31 +0300

    production build

--- app/scripts/build/common/head.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  var module;
-  module = angular.module('KalturaUsageDashboard.head', ['classy']);
-  return module.classy.controller({
-    name: 'HeadCtrl',
-    injectToScope: ['go']
-  });
-})();
